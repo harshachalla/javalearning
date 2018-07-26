2fb64198f2e227536981bf9cea6c8c6d9f917439
harshachalla <harshachalla2@gmail.com>
2018-07-26 19:21:38 +0530

    interview questions

--- src/harshachalla/AlListDemo.java
+++ src/harshachalla/AlListDemo.java
@@ -1,4 +1,14 @@
 package harshachalla;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AlListDemo {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(5, 1, 1, 4, 3, 3, 3);
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+    }
 }
+
