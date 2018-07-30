bdc9cc5716d5dc8d83ee83f9f4cf7ea368030e45
harshachalla <harshachalla2@gmail.com>
2018-07-30 17:55:06 +0530

    adding mylearning java file

---
+++ src/harshachalla/mylearning.java
@@ -0,0 +1,32 @@
+package harshachalla;
+
+public class mylearning {
+    public static void main(String[] args)
+    {
+       // printMyName();
+        hellowWorld();
+    }
+
+    private static void hellowWorld()
+    {
+        //5. Write a simple java program to print “HELLO WORLD”
+        System.out.println("Hello World!");
+    }
+
+    private static void myOperators()
+    {
+        //7. Write a simple program using all operators?
+
+    }
+
+    private static void printMyName()
+    {
+        // 8. Write a simple program using loop statements to print your name 10 times?
+        int i=1;
+        while( i<=10){
+
+            System.out.println("Harsha Challa");
+            i++;
+        }
+    }
+}
